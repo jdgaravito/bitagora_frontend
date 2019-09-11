013b435609017e6882bb1f71cb94559cbbafab6e
Juan David Garavito <jdgaravito317@gmail.com>
2019-09-11 16:28:22 -0500

    added main header

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,15 @@
 <script>
     import MainHeader from './Components/MainHeader.svelte';
 </script>
+<style>
+    :global(:root){
+        --main_focus: #800000;
+        --main_black: #161616;
+        --main_bg: #fcfcfc;
+        --main_light: #fafafa;
+        --main_font: "OpenSans", sans-serif;
+        --header_font: "Montserrat", sans-serif;
+    }
+
+</style>
+<MainHeader />
--- src/Components/MainHeader.svelte
+++ src/Components/MainHeader.svelte
@@ -1,21 +1,140 @@
 <script>
-
+  import FaFacebookF from "svelte-icons/fa/FaFacebookF.svelte";
+  import FaInstagram from "svelte-icons/fa/FaInstagram.svelte";
+  import FaTwitter from 'svelte-icons/fa/FaTwitter.svelte';
 </script>
 
 <style>
-  header {
-    position: fixed;
-    width: 100%;
-    top:0;
-    left:0;
-    height: 4rem;
+  .logo-img {
+    max-width: 300px;
+    padding: 50px 0 50px 0;
+  }
+  .top-wrapper {
+    background: var(--main_black);
+  }
+  .top-container {
     display: flex;
-    justify-content: center;
     align-items: center;
-    box-shadow: 0 2px 6px rgba(0,0,0,0.26)
+    justify-content: space-between;
+  }
+  .top-header {
+    color: var(--main_light);
+    text-transform: uppercase;
+    font-family: var(--header_font);
+  }
+  .top-header a {
+    color: var(--main_light);
+    text-decoration: none;
+    line-height: 2.5rem;
+    font-size: 0.75rem;
+    letter-spacing: 1px;
+    font-weight: 700;
+    padding: 10px 10px 10px 0px;
+  }
+
+  .social_icons {
+    display: flex;
+    align-content: flex-end;
+  }
+  .icon {
+    color: var(--main_light);
+    width: 1.25rem;
+    height: 1.25rem;
+    padding: 0.625rem;
+  }
+
+  .icon a {
+    color: var(--main_light);
+  }
+  .header-inner {
+    background: var(--main_bg);
+  }
+
+  .main-nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: var(--main_black);
+    text-transform: uppercase;
+    font-weight: 700;
+    font-family: var(--header_font);
+    font-size: 1rem;
+    line-height: 10px;
+    letter-spacing: 0.075rem;
+    border-bottom: 0.1875rem solid var(--main_black);
+  }
+
+  .page_menu a {
+    text-decoration: none;
+    color: var(--main_black);
+    padding: 0.625rem;
+  }
+  .page_menu a:hover {
+    background: var(--main_focus);
+    color: var(--main_light);
+  }
+
+  .featured-nav-item {
+    padding: 1rem;
+  }
+  .featured-nav-item:hover {
+    background: var(--main_focus);
+    color: var(--main_light);
   }
 </style>
 
-<header>
-  <h1>Bogotá Gratis</h1>
+<header class="main-header">
+  <div class="wrapper top-wrapper">
+    <div class="inner top-container">
+      <nav class="top-header ">
+        <a href="/">Acerca de</a>
+        <a href="/">Contáctanos</a>
+      </nav>
+      <div class="social_icons">
+        <div class="icon">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/labitagora">
+            <FaFacebookF />
+          </a>
+        </div>
+        <div class="icon">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/labitagora">
+            <FaInstagram />
+          </a>
+        </div>
+        <div class="icon">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/labitagora">
+            <FaTwitter />
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <div class="header">
+    <div class="inner header-inner">
+      <div class="logo-section">
+        <img class="logo-img" src="bitagora-logo.svg" alt="logo-bitagora" />
+      </div>
+      <div class="main-nav">
+        <nav class="page_menu">
+          <a href="/" class="active">Inicio</a>
+          <a href="/">Bogotá Gratis</a>
+          <a href="/">Bici</a>
+          <a href="/">Blog</a>
+        </nav>
+        <div class="featured-nav" />
+        <p class="featured-nav-item">Destacado</p>
+      </div>
+    </div>
+  </div>
 </header>
