36595969371870965b123884d472307a7743abdd
Juan David Garavito <jdgaravito317@gmail.com>
2019-09-11 09:02:35 -0500

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,3 @@
+<script>
+    import MainHeader from './Components/MainHeader.svelte';
+</script>
---
+++ src/Components/MainHeader.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top:0;
+    left:0;
+    height: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgba(0,0,0,0.26)
+  }
+</style>
+
+<header>
+  <h1>Bogotá Gratis</h1>
+</header>
